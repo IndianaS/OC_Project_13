9aecd00503063d0c2f659905aa02808715a15150
Rejaud Benjamin <benjamin.rejaud@gmail.com>
2020-09-10 13:27:39 +0200

    Add tests functionnal

--- tests/test_functionnal.py
+++ tests/test_functionnal.py
@@ -28,3 +28,36 @@ class ChromeFunctionalTestCases(StaticLiveServerTestCase):
             "id_password").send_keys("PasswordTest&7722")
         self.driver.find_element_by_id("button-submit").click()
         logout = self.driver.find_element_by_id("logout-link").click()
+
+
+    def test_user_add_figurine(self):
+        self.driver.find_element_by_id("login-link").click()
+        self.driver.find_element_by_id(
+            "id_username").send_keys("UserTest")
+        self.driver.find_element_by_id(
+            "id_password").send_keys("PasswordTest&7722")
+        self.driver.find_element_by_id("button-submit").click()
+
+        self.driver.find_element_by_id("id-collection").click()
+        self.driver.find_element_by_id("id-addfigurine").click()
+        self.driver.find_element_by_id(
+            "id_figurine_mumber").send_keys("1")
+        self.driver.find_element_by_id(
+            "id_category").send_keys("Super heroes")
+        self.driver.find_element_by_id(
+            "id_name").send_keys("Batman")
+        self.driver.find_element_by_id(
+            "button-submit-addfigurine").click()
+
+    def test_search_friend(self):
+        self.driver.find_element_by_id("login-link").click()
+        self.driver.find_element_by_id(
+            "id_username").send_keys("UserTest")
+        self.driver.find_element_by_id(
+            "id_password").send_keys("PasswordTest&7722")
+        self.driver.find_element_by_id("button-submit").click()
+
+        self.driver.find_element_by_id("id-friends-list").click()
+        self.driver.find_element_by_id(
+            "id_collection_user").send_keys("UserAddTest")
+        self.driver.find_element_by_id("search-friend").click()
