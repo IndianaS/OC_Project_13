df97d47ac03c4c4ca14cd0c5604d7a73fdc260d6
Rejaud Benjamin <benjamin.rejaud@gmail.com>
2020-09-17 13:11:26 +0200

    Test functionnal

--- tests/test_functionnal.py
+++ tests/test_functionnal.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
+from django.shortcuts import reverse
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
@@ -19,6 +20,9 @@ class ChromeFunctionalTestCases(StaticLiveServerTestCase):
             username="UserTest", password="PasswordTest&7722"
         )
 
+    def tearDown(self):
+        self.driver.quit()
+
     def test_user_can_connect_and_disconnect(self):
         self.driver.get(self.live_server_url)
         self.driver.find_element_by_id("login-link").click()
@@ -30,31 +34,24 @@ class ChromeFunctionalTestCases(StaticLiveServerTestCase):
         logout = self.driver.find_element_by_id("logout-link").click()
 
     def test_user_add_figurine(self):
-        self.driver.get(self.live_server_url + "/figurines/add_figurine")
-        self.driver.find_element_by_id("login-link").click()
+        self.driver.get(self.live_server_url + reverse("figurines:add_figurine"))
         self.driver.find_element_by_id("id_username").send_keys("UserTest")
         self.driver.find_element_by_id("id_password").send_keys(
             "PasswordTest&7722"
         )
         self.driver.find_element_by_id("button-submit").click()
-
-        self.driver.find_element_by_id("id-collection").click()
-        self.driver.find_element_by_id("id-addfigurine").click()
-        self.driver.find_element_by_id("id_figurine_mumber").send_keys("1")
+        self.driver.find_element_by_id("id_figurine_number").send_keys("1")
         self.driver.find_element_by_id("id_category").send_keys("Super heroes")
         self.driver.find_element_by_id("id_name").send_keys("Batman")
         self.driver.find_element_by_id("button-submit-addfigurine").click()
 
     def test_search_friend(self):
-        self.driver.get(self.live_server_url + "/users/friends_list")
-        self.driver.find_element_by_id("login-link").click()
+        self.driver.get(self.live_server_url + reverse("users:friends_list"))
         self.driver.find_element_by_id("id_username").send_keys("UserTest")
         self.driver.find_element_by_id("id_password").send_keys(
             "PasswordTest&7722"
         )
         self.driver.find_element_by_id("button-submit").click()
-
-        self.driver.find_element_by_id("id-friends-list").click()
         self.driver.find_element_by_id("collection_user").send_keys(
             "UserAddTest"
         )
