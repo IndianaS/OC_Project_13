05d14385705dbca58e45d3c4f70a2b9b9ca2d992
Rejaud Benjamin <benjamin.rejaud@gmail.com>
2021-01-15 19:25:19 +0100

    Add headless for chrome driver

--- tests/test_functionnal.py
+++ tests/test_functionnal.py
@@ -5,7 +5,8 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 chrome_options = webdriver.ChromeOptions()
-chrome_options.headless = False
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('window-size=1920x1080')
 
 
 class ChromeFunctionalTestCases(StaticLiveServerTestCase):
